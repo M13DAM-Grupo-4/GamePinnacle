ca02b3049c1475b6d11f2c13358f0971ddf7882e
Isaac Cajiao Valle <icv2@outlook.es>
2023-11-29 22:47:24 +0100

    Nueva interfaz de login

--- app/src/main/java/m13dam/grupo4/gamepinnacle/Fragments/LoginMenu.java
+++ app/src/main/java/m13dam/grupo4/gamepinnacle/Fragments/LoginMenu.java
@@ -2,11 +2,20 @@ package m13dam.grupo4.gamepinnacle.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import m13dam.grupo4.gamepinnacle.R;
 
@@ -25,6 +34,20 @@ public class LoginMenu extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // UI
+
+    ImageView email_img;
+    EditText email_text;
+
+    ImageView password_lock;
+    EditText password_text;
+    ImageView password_eye;
+    boolean password_hidden = true;
+
+    ConstraintLayout login_button;
+
+    TextView register_register;
 
     public LoginMenu() {
         // Required empty public constructor
@@ -55,12 +78,43 @@ public class LoginMenu extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_login_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        password_eye = view.findViewById(R.id.password_eye);
+        password_text = view.findViewById(R.id.password_text);
+
+        password_eye.setOnClickListener(v -> {
+
+            if (password_hidden) {
+                password_hidden = false;
+                password_eye.setImageResource(R.drawable.eye_light);
+                int sel_start = password_text.getSelectionStart();
+                int sel_end = password_text.getSelectionEnd();
+                password_text.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                password_text.setSelection(sel_start, sel_end);
+            } else {
+                password_hidden = true;
+                password_eye.setImageResource(R.drawable.key_alt_light);
+                int sel_start = password_text.getSelectionStart();
+                int sel_end = password_text.getSelectionEnd();
+                password_text.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                password_text.setSelection(sel_start, sel_end);
+            }
+
+        });
+
     }
 }
--- app/src/main/java/m13dam/grupo4/gamepinnacle/MainActivity.java
+++ app/src/main/java/m13dam/grupo4/gamepinnacle/MainActivity.java
@@ -1,6 +1,8 @@
 package m13dam.grupo4.gamepinnacle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,84 +15,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import m13dam.grupo4.gamepinnacle.DataBase.DataBaseManager;
+import m13dam.grupo4.gamepinnacle.Fragments.LoginMenu;
 import m13dam.grupo4.gamepinnacle.Types.CurrentSession;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button login;
-    EditText usuario;
-    EditText contraseña;
-    private CheckBox rememberme;
-
+    FragmentContainer mainFragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login = findViewById(R.id.loginBoton);
-        usuario = findViewById(R.id.loginUsuario);
-        contraseña = findViewById(R.id.loginPassword);
-        rememberme = findViewById(R.id.checkBox);
-
-
-        Thread thread = new Thread(() -> {
-            int RememberedID = DataBaseManager.LoginRemember(MainActivity.this);
-            if(RememberedID > 0){
-                CurrentSession.setUserID(RememberedID);
-                Handler handler = new Handler(Looper.getMainLooper());
-
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        abrirNuevaActividad(false);
-                    }
-                });
-            }
-        });
-        thread.start();
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               String usuarioIntroducido = usuario.getText().toString();
-               String contraseñaIntroducido = contraseña.getText().toString();
-
-                Thread thread = new Thread(() -> {
-                    int RememberedID = DataBaseManager.LoginRemember(MainActivity.this);
-                    if(RememberedID > 0){
-                        CurrentSession.setUserID(RememberedID);
-                    } else {
-                        int LoginID = DataBaseManager.Login(usuarioIntroducido,contraseñaIntroducido);
-                        CurrentSession.setUserID(LoginID);
-                    }
-                    Handler handler = new Handler(Looper.getMainLooper());
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            abrirNuevaActividad(rememberme.isChecked());
-                        }
-                    });
-
-                });
-
-            }
-        });
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_fragment_container, LoginMenu.class, null)
+                .commit();
 
     }
-    private void abrirNuevaActividad(Boolean remember) {
 
-        if (CurrentSession.getUserID() > 0) {
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            startActivity(intent);
-            if (remember){
-                DataBaseManager.SaveLoginRemember(CurrentSession.getUserID(), MainActivity.this);
-            }
-            finish();
-        } else {
-            Toast.makeText(MainActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
